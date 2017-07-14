705e877b338dc90558426fd00fb64c67cc7672f2
kvinash <kapilyadav93@gmail.com>
2017-07-14 17:46:35 +0530

    lintsetup

--- src/index.js
+++ src/index.js
@@ -1,46 +1,42 @@
-import { combineReducers, createStore } from 'redux'
+import { combineReducers, createStore } from 'redux';
 
 
-var increReucer = (state={}, action) => {
-    if (action.type === "increment") {
-        state.incre = action.payload+1;
-        
-    }
-    return state;
-}
-var decreReucer = (state={}, action) => {
-    if (action.type ==="decrement") {
-        state.decre = action.payload-1;
-        
-    }
-    return state;
-}
-var pushNameReducer = (state=[], action) => {
-    if (action.type ==="pushnames") {
-        state.push(action.payload)
-        
-    }
-    return state;
-}
-var reducers = combineReducers({
-    increment: increReucer,
-    decrement: decreReucer,
-    names: pushNameReducer
-})
+const increReucer = (state = {}, action) => {
+  if (action.type === 'increment') {
+    state.incre = action.payload + 1;
+  }
+  return state;
+};
+const decreReucer = (state = {}, action) => {
+  if (action.type === 'decrement') {
+    state.decre = action.payload - 1;
+  }
+  return state;
+};
+const pushNameReducer = (state = [], action) => {
+  if (action.type === 'pushnames') {
+    state.push(action.payload);
+  }
+  return state;
+};
+const reducers = combineReducers({
+  increment: increReucer,
+  decrement: decreReucer,
+  names: pushNameReducer,
+});
 
-var store = createStore(reducers, {
-    increment: {
-        incre: 1
-    },
-    decrement: {
-        decre: 10
-    },
-    names:[]
-},window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(reducers, {
+  increment: {
+    incre: 1,
+  },
+  decrement: {
+    decre: 10,
+  },
+  names: [],
+}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 store.subscribe(() => {
-    console.log("HELLO", store.getState());
-
-})
-store.dispatch({ type: "increment", payload: "1" })
-store.dispatch({ type: "decrement", payload: "1" })
-store.dispatch({ type: "pushnames", payload: "harsh" })
+  console.log('HELLO', store.getState());
+});
+store.dispatch({ type: 'increment', payload: '1' });
+store.dispatch({ type: 'decrement', payload: '1' });
+store.dispatch({ type: 'pushnames', payload: 'harsh' });
--- src/index2.js
+++ src/index2.js
@@ -9,45 +9,45 @@ import { combineReducers, createStore } from 'redux';
 
 //  }
 // const userState = {
- 
+
 // }
-const userReducer = (state={}, actions) =>{
-    switch(actions.type){
-        case "CHANGE_NAME":{
-            state.name = actions.payload;
-            break;
-        }
-        case "CHANGE_AGE":{
-            state.age = actions.payload;
-            break;
-        }
+const userReducer = (state = {}, actions) => {
+  switch (actions.type) {
+    case 'CHANGE_NAME': {
+      state.name = actions.payload;
+      break;
     }
- return state
+    case 'CHANGE_AGE': {
+      state.age = actions.payload;
+      break;
+    }
+  }
+  return state;
 };
-const tweetReducer = (state=[], actions) =>{
-    switch(actions.type){
-        case "ADD_TWEET":{
-            state.push(actions.payload);
-            break;
-        }
+const tweetReducer = (state = [], actions) => {
+  switch (actions.type) {
+    case 'ADD_TWEET': {
+      state.push(actions.payload);
+      break;
     }
-    return state
+  }
+  return state;
 };
 const reducers = combineReducers({
-    user: userReducer,
-    tweets : tweetReducer, 
-})
- const store = createStore(reducers, {
-     user : {
-         name : "will",
-         age:35,
-     },
-     tweets:[]
- }, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+  user: userReducer,
+  tweets: tweetReducer,
+});
+const store = createStore(reducers, {
+  user: {
+    name: 'will',
+    age: 35,
+  },
+  tweets: [],
+}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
- store.subscribe(()=>{
-     console.log("store changed", store.getState())
- })
- store.dispatch({type:"CHANGE_NAME", payload:"phil"}) 
- store.dispatch({type:"CHANGE_AGE", payload:"30"})
- store.dispatch({type:"ADD_TWEET", payload:"Hello REACT!!!"})
+store.subscribe(() => {
+  console.log('store changed', store.getState());
+});
+store.dispatch({ type: 'CHANGE_NAME', payload: 'phil' });
+store.dispatch({ type: 'CHANGE_AGE', payload: '30' });
+store.dispatch({ type: 'ADD_TWEET', payload: 'Hello REACT!!!' });
